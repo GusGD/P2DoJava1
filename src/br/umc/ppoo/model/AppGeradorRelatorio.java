ea6e11f1c5ab00c53bd0bf38839e32b9f1a8f637
guilherme terriaga <guilherme.terriaga@gmail.com>
2020-12-02 21:32:15 -0300

    Finalizado o segundo diagrama de sequencia

--- src/br/umc/ppoo/model/AppGeradorRelatorio.java
+++ src/br/umc/ppoo/model/AppGeradorRelatorio.java
@@ -12,6 +12,8 @@ package br.umc.ppoo.model;
 public class AppGeradorRelatorio {
     //main
     public static void main(String[] args) {
+        GeradorDeRelatorio geradorDeRelatorio = new GeradorDeRelatorio();
         
+        geradorDeRelatorio.gerar(Status.Pendente);
     }
 }
--- src/br/umc/ppoo/model/GeradorDeRelatorio.java
+++ src/br/umc/ppoo/model/GeradorDeRelatorio.java
@@ -5,12 +5,25 @@
  */
 package br.umc.ppoo.model;
 
+import br.umc.ppoo.model.template.RelatorioTemplate;
+import java.util.ArrayList;
+
 /**
  *
  * @author guilherme
  */
 public class GeradorDeRelatorio {
     public void gerar (Status status){
+        LeitorArquivo leitorArquivo = new LeitorArquivo();
+        RelatorioFactory relatorioFactory = new RelatorioFactory();
+        ArrayList<Pedido> pedidos = new ArrayList<>();
+        for (Pedido pedido : leitorArquivo.ler()){
+            if(pedido.getStatus() == status){
+                pedidos.add(pedido);
+            }
+        }
         
+        RelatorioTemplate relatorioTemplate = relatorioFactory.create(status);
+        relatorioTemplate.gerar(pedidos);
     }
 }
